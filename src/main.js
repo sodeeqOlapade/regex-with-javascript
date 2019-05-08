b769e7f6139f8e85ce4a00557417477154a2adf8
sodeeq olapade <olapadeabiodun21@email.com>
2019-05-08 13:09:11 +0100

    implements isQuestion method and associated tests

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-//add 'hasVowels' to String prototypes
+//adds 'hasVowels' to String prototypes
 String.prototype.hasVowels = function() {
-    var regExp = /[aeiou]/g;
-    
-    return regExp.test(this);
+  var regExp = /[aeiou]/g; //regExp pattern to match vowels
+  return regExp.test(this); //returns the result of invoking test method on regExp and passing target string as pararmeter to test
 };
 
+//adds 'isQuestion' to Stirng prototypes
+String.prototype.isQuestion = function() {
+  var regExp = /[\w\s]+\?$/; //regular expression pattern to match '?' at the end of a string
+  return regExp.test(this); //returns the result of invoking test method on regExp and passing target string as pararmeter to test
+};
 module.exports = { String };
+
--- tests/Unit/src/main.test.js
+++ tests/Unit/src/main.test.js
@@ -9,4 +9,13 @@ describe("String Custom Methods", () => {
         expect('script'.hasVowels()).toBe(true);
       });
   });
+
+  describe("isQuestion", () => {
+    it("returns false as 'What is your name' doesn't end with ?", () => {
+      expect('What is your name'.isQuestion()).toBe(false);
+    });
+    it("returns true as 'What is your name?' ends with ?", () => {
+        expect('What is your name?'.isQuestion()).toBe(true);
+      });
+  });
 });
